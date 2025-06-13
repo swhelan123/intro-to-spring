6f7b3d1b3be29afabfcd6d295ca91698fe6cd3ff
Shane Whelan <swhelan@twilio.com>
2025-06-13 20:20:58 +0100

    add POJO for greeting

---
+++ src/main/java/com/springintro/introtospring/Greeting.java
@@ -0,0 +1,22 @@
+package com.springintro.introtospring;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    //constructor
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    //getters
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
--- src/main/java/com/springintro/introtospring/HelloWorldController.java
+++ src/main/java/com/springintro/introtospring/HelloWorldController.java
@@ -1,19 +1,35 @@
 package com.springintro.introtospring;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping
+    @GetMapping
     public String helloWorld() {
         return "Hello World from Spring Boot";
     }
 
-    @RequestMapping("/goodbye")
+    @GetMapping("/goodbye")
     public String goodbye() {
         return "Goodbye World from Spring Boot";
+    }
+
+//    @GetMapping("/greeting")
+//    public String sayGreeting(@RequestParam String name) {
+//        return "Hello " + name + "!";
+//    }
+
+    @GetMapping("/greeting")
+    public Greeting sayGreeting(@RequestParam(defaultValue = "World") String name) {
+        //new greeting object, give it id of 1 and build content string like before
+        return new Greeting(1L, "Hello " + name + "!");
+
+
     }
 
 }
