df3216cabc074a899cecae2d0da520c0519b7146
Shane Whelan <swhelan@twilio.com>
2025-06-13 19:14:45 +0100

    Init commit

---
+++ src/main/java/com/springintro/introtospring/IntroToSpringApplication.java
@@ -0,0 +1,13 @@
+package com.springintro.introtospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroToSpringApplication.class, args);
+    }
+
+}
